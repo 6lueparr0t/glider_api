44bf78bbda761aaa3500a5f31514bee5adb18eda
joyozzz <t08361@naver.com>
2021-05-28 19:26:59 +0900

    react-router 연동하여 기본페이지 생성

---
+++ src/main/view/src/pages/Ex.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Ex = () => {
+    return (
+        <div>
+            <h2>
+                예시
+            </h2>
+        </div>
+    );
+};
+
+export default Ex;
